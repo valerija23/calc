b22b96888e362e8a8967fa5e2ff2d0d6d2e2f6dc
Valerija Januskevica <you@example.com>
2023-12-13 10:20:07 +0200

    Visi uzdevumi izņemot pedējo

--- calc.py
+++ calc.py
@@ -1,22 +1,24 @@
 
 # konvertē datu mērvienības
 def megabit_to_megabyte(megabit):
-   result = 0
+   result = megabit*8
    return result
 
 # saskaiti degvielas patēriņu uz 100 km
-def fuel_consumption(litres, kilometers):
-   result = 0
+def fuel_consumption(litres, kilometers = 100):
+   result = (litres/kilometers)*100
    return result
 
 # konvertē temperatūru
 def celsius_to_fahrenheit(celsius):
-    result = 0
+    result = (celsius)*9/5+32
     return result
 
 # saliec visus skaitļus pirms dota skaitļa (izmanto for)
 def sigma(tail):
     result = 0
+    for i in range(tail+1):
+       result += i
     return result
 
 # nokonvertē svaru uz tuvāko mērvienību - grams, kilograms, tonna (izmanto if)
